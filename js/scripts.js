a5a2f000240496b8d0e7d65ebf28d207e6642917
= <seanparsons2003@gmail.com>
2024-10-10 15:11:37 +0100

    add canvas and japanese language support

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,22 @@
-console.log(`Linked successfully`)
+console.log(`Linked successfully`);
+let pagelanguage=`en`;
+
+document.getElementById("language-select").onchange = function (){
+    console.log(`called translate function`);
+    if (pagelanguage==`en`) {
+        document.getElementById(`page-header`).textContent = (`HTML5のグラフィカルキャンバスに描きましょう`);
+        document.getElementById(`language-select-label`).textContent = (`言語を選ぶ:`);
+        document.getElementById(`colour-label`).textContent = (`色:`);
+        document.getElementById(`width-label`).textContent = (`太さ:`);
+        document.getElementById(`clear`).textContent = (`消す`);
+        pagelanguage=`jp`;
+    } else {
+        document.getElementById(`page-header`).textContent = (`Let's Draw on a HTML5 Canvas`);
+        document.getElementById(`language-select-label`).textContent = (`Select a language:`);
+        document.getElementById(`colour-label`).textContent = (`Colour:`);
+        document.getElementById(`width-label`).textContent = (`Width:`);
+        document.getElementById(`clear`).textContent = (`Clear`);
+        pagelanguage=`en`;
+    }
+
+}
